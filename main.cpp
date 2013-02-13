e2e54c018ff4d45dbb96ee2e1ac2edca24c62fad
Andres G. Saravia <ags3006@member.fsf.org>
2013-02-13 16:36:20 -0600

    I use now an input file for the arguments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -22,7 +24,7 @@ int state_label (int e1, int e2, int ir, int ram, int n_ir)
 }
 
 
-int main (void) {
+int main (int argc, char *argv[]) {
   int ir_phonons, raman_phonons, size;
   double band_energy[3];
   double nn_hopping, on_site_repulsion, ir_energy, raman_energy, raman_shift;
@@ -31,36 +33,64 @@ int main (void) {
   int e1, e2, ir, ram, row, col, n;  // just counters
   double energy;  // useful variable to fill in the hamiltonian elements
 
-  cout << endl << "This program calculates the eigenvalues for a model hamiltonian" << endl;
-  cout << "representing a linear cluster of 3 atoms with 2 electrons " << endl;
-  cout << "hopping between them." << endl << endl;
+  ifstream inputfile;
+  string line;
+  inputfile.open("parameters.txt");
+  if (inputfile.is_open()) {
+    // TODO: Assert that the values make sense
+    getline(inputfile, line, ',');
+    band_energy[0] = atof(line.c_str());
+    getline(inputfile, line);
 
-  // TODO: Assert that the values make sense
-  cout << "Please enter the following parameters:" << endl;
-  cout << "Band energy for site 1: ";
-  cin >> band_energy[0];
-  cout << "Band energy for site 2: ";
-  cin >> band_energy[1];
-  cout << "Band energy for site 3: ";
-  cin >> band_energy[2];
-  cout << "Nearest neighbor hopping: ";
-  cin >> nn_hopping;
-  cout << "On site Coulomb repulsion: ";
-  cin >> on_site_repulsion;
-  cout << "Infrared phonon's energy: ";
-  cin >> ir_energy;
-  cout << "Electron - infrared phonons coupling: ";
-  cin >> e_ir_coupling;
-  cout << "Raman phonon's energy: ";
-  cin >> raman_energy;
-  cout << "Electron - raman phonons coupling: ";
-  cin >> e_ram_coupling;
-  cout << "Raman shift: ";
-  cin >> raman_shift;
-  cout << "Number of infrared phonons: ";
-  cin >> ir_phonons;
-  cout << "Number of Raman phonons: ";
-  cin >> raman_phonons;
+    getline(inputfile, line, ',');
+    band_energy[1] = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    band_energy[2] = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    nn_hopping = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    on_site_repulsion = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    ir_energy = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    e_ir_coupling  = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    raman_energy = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    e_ram_coupling = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    raman_shift = atof(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    ir_phonons = atoi(line.c_str());
+    getline(inputfile, line);
+
+    getline(inputfile, line, ',');
+    raman_phonons = atoi(line.c_str());
+    getline(inputfile, line);
+  }
+  else {
+    cout << "Input file not found. I will create a template for you. " << endl;
+    double temp[3] = {0, 0, 0};
+    save_parameters(temp, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0);
+  }
 
   size = 9 * (1 + raman_phonons) * (1 + ir_phonons);
   cout << "The size of the hamiltonian is: " << size << "x" << size << endl;
@@ -165,12 +195,9 @@ int main (void) {
     }
   }
 
-  cout << "I will try to calculate the eigenvalues and eigenvectors now. This could take some time... ";
+  cout << "I will try to calculate the eigenvalues and eigenvectors now." << endl;
+  cout << "This could take some time... ";
   MyEigenSolver eigensolver(h);
-  cout << "Done." << endl;
-
-  cout << "Saving the parameters used for this calculation at \"parameters.txt\"... ";
-  save_parameters(band_energy, nn_hopping, on_site_repulsion, ir_energy, e_ir_coupling, raman_energy, e_ram_coupling, raman_shift, ir_phonons, raman_phonons);
   cout << "Done." << endl;
 
   cout << "Saving the eigenvalues at \"eigenvalues.txt\"... ";
